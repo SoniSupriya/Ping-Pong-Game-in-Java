4ff3ca5a1d0d23da38c4158ff126bef356343d75
SoniSupriya <sonisupriya2005@gmail.com>
2025-05-10 16:21:14 +0530

    added the necessary imports to the files

--- PingPong.java
+++ PingPong.java
@@ -3,6 +3,8 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class Paddle {
-    
+public class PingPong{
+    public static void main(String[] args) {
+        
+    }
 }
